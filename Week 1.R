d34307d84846a403a27200f821bf50077770df43
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2024-01-15 19:17:21 +0300

    Updated recap codes

--- Week 1.R
+++ Week 1.R
@@ -1,5 +1,29 @@
 #Week 1
-print("Hello World.")
-for(i in 1:5){
-  cat(i,"*",i,"=",i*i,"\n")
-}
+print("I love Kenya")
+cat("I love Kenya")
+
+mystring1 <- "I am "
+mystring2 <- "years old.\n"
+years = 18
+
+print(mystring1)
+cat(mystring1," ",years," ",mystring2,"\n")
+cat("I am ",years," old, give me a beer")
+
+
+set.seed(2024)
+
+A <- matrix(round(rnorm(35,5,2),0),nrow = 7,byrow = T)
+A
+
+
+K <- list("This is a recup",cos(25),v=c(2,5,6,3),exp(60))
+K
+
+j -> cos(20)
+cos(20) ->j
+
+k <- c(3,2,5)
+j = c(2,6,5)
+
+j>k
